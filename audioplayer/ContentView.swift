11a65a056bcb808747abe61db7757d447fe3af23
Ronnie Bumgarner <ronniebumg@icloud.com>
2023-11-25 12:11:05 -0800

    Initial Commit

---
+++ audioplayer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  audioplayer
+//
+//  Created by RONNIE BUMGARNER on 11/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ audioplayer/audioplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  audioplayerApp.swift
+//  audioplayer
+//
+//  Created by RONNIE BUMGARNER on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct audioplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
